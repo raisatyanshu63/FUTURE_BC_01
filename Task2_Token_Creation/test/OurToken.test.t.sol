1a9e1226657cef525b6e0fe1efdd5ba1ad8942df
Rai Satyanshu <anshumank141@gmail.com>
2025-10-15 10:42:13 +0000

    Add Task2_Token_Creation as normal folder

---
+++ Task2_Token_Creation/test/OurToken.test.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {OurToken} from "../src/OurToken.sol";
+import {DeployOurToken} from "../script/DeployOurToken.s.sol";
+
+contract OurTokenTest is Test {
+    OurToken public ourToken;
+    DeployOurToken public deployer;
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
+
+    uint256 public constant STARTING_BALANCE = 100 ether;
+
+    function setUp() public {
+        deployer = new DeployOurToken();
+        ourToken = deployer.run();
+
+        vm.prank(msg.sender);
+        ourToken.transfer(bob, STARTING_BALANCE);
+    }
+
+    function testBobBalance() public view {
+        assertEq(STARTING_BALANCE, ourToken.balanceOf(bob));
+    }
+
+    function testAllowanceWorks() public {
+        uint256 initialAllowance = 100 ether;
+
+        vm.prank(bob);
+        ourToken.approve(alice, initialAllowance);
+
+        uint256 transferAmount = 50 ether;
+
+        vm.prank(alice);
+        ourToken.transferFrom(bob, alice, transferAmount);
+
+        assertEq(ourToken.balanceOf(alice), transferAmount);
+        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
+    }
+
+    function testTransferFromExceedAllowanceReverts() public {
+        vm.prank(bob);
+        ourToken.approve(alice, 10 ether);
+
+        vm.prank(alice);
+        vm.expectRevert(); // Trying to transfer 20 > 10
+        ourToken.transferFrom(bob, alice, 20 ether);
+    }
+}
